7462bfa1bb753ababcd07dea79ea10d2b1d8679c
EmerBV <emersmc@hotmail.com>
2023-06-28 22:01:10 +0200

    AuthenticationVC & SearchVC UI

--- Swiftagram/Core/Search/SearchView.swift
+++ Swiftagram/Core/Search/SearchView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 12) {
+                    ForEach(0...10, id: \.self) { user in
+                        HStack {
+                            Image("emptyuser")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                                .background(.gray)
+                            .clipShape(Circle())
+                            
+                            VStack(alignment: .leading) {
+                                Text("username")
+                                    .fontWeight(.semibold)
+                                
+                                Text("name")
+                            }
+                            .font(.footnote)
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                .padding(.top, 8)
+                .searchable(text: $searchText, prompt: "Search...")
+            }
+        }
     }
 }
 
