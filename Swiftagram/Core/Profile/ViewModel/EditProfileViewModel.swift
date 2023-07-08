060a4dc459eb06f3d8aca0fc5a3ebbca111211eb
EmerBV <emersmc@hotmail.com>
2023-07-09 01:22:26 +0200

    + Upload Profile Image

--- Swiftagram/Core/Profile/ViewModel/EditProfileViewModel.swift
+++ Swiftagram/Core/Profile/ViewModel/EditProfileViewModel.swift
@@ -21,6 +21,8 @@ class EditProfileViewModel: ObservableObject {
     @Published var fullname = ""
     @Published var bio = ""
     
+    private var uiImage: UIImage?
+    
     init(user: User) {
         self.user = user
     }
@@ -30,12 +32,18 @@ class EditProfileViewModel: ObservableObject {
         
         guard let data = try? await item.loadTransferable(type: Data.self) else { return }
         guard let uiImage = UIImage(data: data) else { return }
+        self.uiImage = uiImage
         self.profileImage = Image(uiImage: uiImage)
     }
     
     func updateUserData() async throws {
         // Update profile image if change
         var data = [String: Any]()
+        
+        if let uiImage = uiImage {
+            let imageUrl = try? await ImageUploader.uploadImage(image: uiImage)
+            data["profileImageUrl"] = imageUrl
+        }
         
         // Update name if change
         if !fullname.isEmpty && user.fullname != fullname {
